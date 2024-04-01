f0978b6d644bd1d7622b6ae451e45ea77bd1801f
Manuel <manuelvictor36791@gmail.com>
2024-04-01 14:43:48 +0200

    Add remember me functionality and handle no internet view
    - Added remember me functionality to store email and password in cache
    - Implemented handling for no internet view when internet connection is lost
    
    Signed-off-by: Manuel <manuelvictor36791@gmail.com>

--- lib/main_view.dart
+++ lib/main_view.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:gap/gap.dart';
+import 'package:loveleta/core/utils/extensions.dart';
 
+import 'core/dependency_injection/di.dart' as di;
+import 'core/router/router.dart';
+import 'core/shared/arguments.dart';
 import 'core/shared/cubits/internet_checker_cubit.dart';
-import 'core/utils/app_text_styles.dart';
-import 'core/utils/app_images.dart';
-import 'features/auth/login/presentation/pages/login_view.dart';
+import 'core/utils/app_colors.dart';
+import 'features/auth/login/presentation/manager/login_cubit.dart';
+import 'features/no_internet_view.dart';
+import 'generated/l10n.dart';
 
 class MainView extends StatefulWidget {
   const MainView({super.key});
@@ -19,56 +21,52 @@ class MainView extends StatefulWidget {
 class _MainViewState extends State<MainView> {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => InternetCubit(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => InternetCubit(),
+        ),
+        BlocProvider(
+          create: (context) => di.di<LoginCubit>()..rememberMe(),
+        ),
+      ],
       child: BlocConsumer<InternetCubit, InternetStates>(
-        listener: (context, state) {
-          // if (state == InternetStates.gained) {
-          // } else if (state == InternetStates.lost) {
-          //   context.defaultSnackBar(
-          //     "No Internet Connection",
-          //     color: AppColors.errorColor,
-          //   );
-          // }
-        },
+        listener: (context, state) {},
         builder: (context, state) {
-          return state == InternetStates.lost
-              ? Scaffold(
-                  body: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(
-                        AppImages.wifiSvg,
-                        height: 135.h,
-                        width: 150.w,
-                      ),
-                      Gap(20.h),
-                      Text(
-                        "No Internet Connection",
-                        textAlign: TextAlign.center,
-                        style: CustomTextStyle.kTextStyleF18,
-                      ),
-                      Gap(10.h),
-                      Text(
-                        "Make sure your phone is connected to the Internet",
-                        textAlign: TextAlign.center,
-                        style: CustomTextStyle.kTextStyleF14,
-                      ),
-                    ],
-                  ),
-                )
-              : const LoginView();
-
+          return BlocConsumer<LoginCubit, LoginStates>(
+            listener: (context, loginState) {
+              loginState.maybeWhen(
+                success: (state) async {
+                  if (state.status == 1) {
+                    context.pushNamed(bottomNavBar);
+                  } else if (state.status == 0 &&
+                      state.msg ==
+                          "Active your account first verification code sent to your email !") {
+                    context.pushNamed(
+                      verifyAccountPageRoute,
+                      arguments: VerifyAccountArgs(email: state.email!),
+                    );
+                  } else {
+                    context.pushNamed(loginPageRoute);
+                  }
+                },
+                error: (errCode, err) {
+                  context.defaultSnackBar(
+                    S.of(context).error(errCode, err),
+                    color: AppColors.errorColor,
+                  );
+                },
+                orElse: () {},
+              );
+            },
+            builder: (context, loginState) {
+              return state == InternetStates.gained
+                  ? Image.asset("assets/images/splash.png", fit: BoxFit.fitWidth,)
+                  : const NoInternetView();
+            },
+          );
         },
       ),
     );
   }
-
-// rememberMe(String email, String pass) async {
-//   String email = await CacheHelper.getData("email");
-//   String pass = await CacheHelper.getData("pass");
-//
-//
-// }
 }
